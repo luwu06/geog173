0854b8d76eb35ec4b1d7332984307097170c3a05
Matt Roe <mattroe.mail@gmail.com>
2013-05-15 14:19:15 -0700

    minor updates to python script

---
+++ scripts/flood_test.py
@@ -0,0 +1,13 @@
+###################################################################################################################
+## flood_test.py
+## GEOG 173 Final Project: Flood Modeling
+## Script Description: Takes input volume and projects affected flood area
+#################################################################################################################
+
+import arcpy
+folder_path = "~/Developer/geog173/data/"
+arcpy.env.workspace = folder_path
+arcpy.env.overwriteOutput = True
+
+
+
